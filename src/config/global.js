f1c1c12b4954f5c23e24c00fc5bb16ff9046587b
Sebastian Trujillo <129899570+strujilloa@users.noreply.github.com>
2025-11-10 16:02:53 -0500

    Update CFA & Vídeo

--- src/config/global.js
+++ src/config/global.js
@@ -195,7 +195,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/CFA_XX_DU.pdf',
+        download: 'downloads/CFA_02_2720213_DU.pdf',
       },
       {
         icono: 'fas fa-download',
--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -25,7 +25,7 @@
       .col-lg-12
         figure
           .video
-            iframe(width="560" height="315" src="https://www.youtube.com/embed/vdPrCjWJSHo?si=X4NxENRo3LLXAua_" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/1PdXnorZ-uI?si=-BMq8AKJiFvvOU1f" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
 </template>
 
